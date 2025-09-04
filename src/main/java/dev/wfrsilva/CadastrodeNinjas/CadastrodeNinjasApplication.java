4abc18ca52d8a706ad8fc4c5cdf96e4a9680ee50
wfrsilva <wfrsilva@gmail.com>
2025-09-04 00:25:05 -0300

    Commit inicial do projeto

---
+++ src/main/java/dev/wfrsilva/CadastrodeNinjas/CadastrodeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.wfrsilva.CadastrodeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrodeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrodeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wfrsilva/CadastrodeNinjas/CadastrodeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wfrsilva.CadastrodeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrodeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
