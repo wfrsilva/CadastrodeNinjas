6ecf77dbea709e47f780b866a82dc4b244d25544
wfrsilva <wfrsilva@gmail.com>
2025-09-04 15:45:12 -0300

    Atualizando NinjaModel
     On branch main
     Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/main/java/dev/wfrsilva/cadastrodeNinjas/NinjaModel.java

--- src/main/java/dev/wfrsilva/cadastrodeNinjas/NinjaModel.java
+++ src/main/java/dev/wfrsilva/cadastrodeNinjas/NinjaModel.java
@@ -8,18 +8,43 @@ public class NinjaModel {
     
     @Id
     @GenerateValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome;
-    String email;
-    int idade;
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
 
     public NinjaModel(){
     }// construtor vazio
 
-    public Ninjamodel(String nome, Stirng email, int idade){
+    public NinjaModel(String nome, String email, int idade){
         this.nome = nome;
         this.email = email;
         this.idade = idade;
     }// construtor completo
+
+
+    public String getNome(){
+        return this.nome;
+    } //getNome
+
+    public String getEmail(){
+        return this.email;
+    } //getEmail
+
+    public int getIdade(){
+        return this.idade;
+    }//getIdade
+
+    public void setNome(String nome){
+        this.nome = nome;        
+    }//setNome
+
+    public void setEmail(String email){
+        this.email = email;
+    }//setEmail
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }//setIdade
 
 } //NinjaModel
